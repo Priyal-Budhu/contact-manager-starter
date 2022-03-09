821a6313ea52a3f9dca4ab2bcfd3d6e46ba67273
Priyal Budhu <pbudhu@jhb.dvt.co.za>
2022-03-09 15:47:21 +0200

    Finished 2

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,19 +1,6 @@
 package com.programming.techie;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,6 +8,9 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -42,7 +32,6 @@ public class ContactManagerTest {
 
     @Test
     @DisplayName("Should Create Contact")
-    @EnabledOnOs(value = OS.MAC, disabledReason = "Should Run only on MAC")
     public void shouldCreateContact() {
         contactManager.addContact("John", "Doe", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
@@ -74,28 +63,18 @@ public class ContactManagerTest {
     }
 
     @Test
-    @DisplayName("Test Contact Creation on Developer Machine")
-    public void shouldTestContactCreationOnDEV() {
-        Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")));
+    @DisplayName("Should Create Contact")
+    @EnabledOnOs(value = OS.MAC, disabledReason = "Should Run only on MAC")
+    public void shouldCreateContactOnMAC() {
         contactManager.addContact("John", "Doe", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
     }
 
-    @AfterEach
-    public void tearDown() {
-        System.out.println("Should Execute After Each Test");
-    }
-
-    @AfterAll
-    public static void tearDownAll() {
-        System.out.println("Should be executed at the end of the Test");
-    }
-
-    @DisplayName("Repeat Contact Creation Test 5 Times")
-    @RepeatedTest(value = 5,
-            name = "Repeating Contact Creation Test {currentRepetition} of {totalRepetitions}")
-    public void shouldTestContactCreationRepeatedly() {
+    @Test
+    @DisplayName("Test Contact Creation on Developer Machine")
+    public void shouldTestContactCreationOnDEV() {
+        Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")));
         contactManager.addContact("John", "Doe", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
@@ -109,13 +88,17 @@ public class ContactManagerTest {
         assertEquals(1, contactManager.getAllContacts().size());
     }
 
-    @DisplayName("Phone Number should match the required Format")
-    @ParameterizedTest
-    @ValueSource(strings = {"0123456789", "1234567890", "+0123456789"})
-    public void shouldTestPhoneNumberFormat(String phoneNumber) {
-        contactManager.addContact("John", "Doe", phoneNumber);
-        assertFalse(contactManager.getAllContacts().isEmpty());
-        assertEquals(1, contactManager.getAllContacts().size());
+    @Nested
+    class RepeatedTests {
+
+        @DisplayName("Repeat Contact Creation Test 5 Times")
+        @RepeatedTest(value = 5,
+                name = "Repeating Contact Creation Test {currentRepetition} of {totalRepetitions}")
+        public void shouldTestContactCreationRepeatedly() {
+            contactManager.addContact("John", "Doe", "0123456789");
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
     }
 
     @Nested
@@ -123,7 +106,7 @@ public class ContactManagerTest {
 
         @DisplayName("Phone Number should match the required Format")
         @ParameterizedTest
-        @ValueSource(strings = {"0123456789", "1234567890", "+0123456789"})
+        @ValueSource(strings = {"0123456789", "0123456798", "0123456897"})
         public void shouldTestPhoneNumberFormatUsingValueSource(String phoneNumber) {
             contactManager.addContact("John", "Doe", phoneNumber);
             assertFalse(contactManager.getAllContacts().isEmpty());
@@ -132,7 +115,7 @@ public class ContactManagerTest {
 
         @DisplayName("CSV Source Case - Phone Number should match the required Format")
         @ParameterizedTest
-        @CsvSource({"0123456789", "1234567890", "+0123456789"})
+        @CsvSource({"0123456789", "0123456798", "0123456897"})
         public void shouldTestPhoneNumberFormatUsingCSVSource(String phoneNumber) {
             contactManager.addContact("John", "Doe", phoneNumber);
             assertFalse(contactManager.getAllContacts().isEmpty());
@@ -149,6 +132,19 @@ public class ContactManagerTest {
         }
     }
 
+    @DisplayName("Method Source Case - Phone Number should match the required Format")
+    @ParameterizedTest
+    @MethodSource("phoneNumberList")
+    public void shouldTestPhoneNumberFormatUsingMethodSource(String phoneNumber) {
+        contactManager.addContact("John", "Doe", phoneNumber);
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    private static List<String> phoneNumberList() {
+        return Arrays.asList("0123456789", "0123456798", "0123456897");
+    }
+
     @Test
     @DisplayName("Test Should Be Disabled")
     @Disabled
@@ -156,4 +152,13 @@ public class ContactManagerTest {
         throw new RuntimeException("Test Should Not be executed");
     }
 
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Should Execute After Each Test");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        System.out.println("Should be executed at the end of the Test");
+    }
 }
